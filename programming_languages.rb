1fa1e05aee408a4003aa5ac4113c7b07986f7c39
zhifeng ou <zhifengou@hotmail.com>
2018-09-05 17:42:50 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+
 def reformat_languages(languages)
-  # your code here
+hash = {}
+languages.each do |k, v| 
+    v.each do |k1, v1| 
+        if hash[k1]
+            hash[k1][:style] << k
+        else
+            hash[k1] = {type: v1[:type], style: [k]}
+        end
+    end
+end
+hash
 end
